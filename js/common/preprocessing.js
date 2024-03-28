4bc3ca4ad2a0d09660940cc2421172ddec912693
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-03-28 19:09:26 -0400

    Ajout fichier de données

---
+++ js/common/preprocessing.js
@@ -0,0 +1,29 @@
+/**
+ * Finds the names of the players who had at least 2 goals in the tournament
+ *
+ * @param {object[]} data The dataset containing all the players in the tournament
+ * @returns {string[]} The names of the players with at least 2 goals in the tournament
+ */
+export function getTopPlayersGoals (data) {
+
+}
+
+/**
+ * Finds the names of the players who had at least 2 assists in the tournament
+ *
+ * @param {object[]} data The dataset containing all the players in the tournament
+ * @returns {string[]} The names of the players with at least 2 assists in the tournament
+ */
+export function getTopPlayersAssists (data) {
+
+}
+
+/**
+ * Finds the names of the players who had at least 3 goals + assists in the tournament
+ *
+ * @param {object[]} data The dataset containing all the players in the tournament
+ * @returns {string[]} The names of the players with at least 3 goals + assists in the tournament
+ */
+export function getTopPlayersGoalsAndAssists (data) {
+
+}
