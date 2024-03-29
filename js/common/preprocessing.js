b771de9737902cb9ef0a9783d8921660b452785b
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-03-29 14:11:20 -0400

    Preprocessing viz1

--- js/common/preprocessing.js
+++ js/common/preprocessing.js
@@ -5,7 +5,13 @@
  * @returns {string[]} The names of the players with at least 2 goals in the tournament
  */
 export function getTopPlayersGoals (data) {
-
+    const topPlayersGoals = []
+    data.forEach(player => {
+        if (player.Goals >= 2) {
+        topPlayersGoals.push(player.Player)
+        }
+    })
+    return topPlayersGoals
 }
 
 /**
@@ -15,7 +21,13 @@ export function getTopPlayersGoals (data) {
  * @returns {string[]} The names of the players with at least 2 assists in the tournament
  */
 export function getTopPlayersAssists (data) {
-
+    const topPlayersAssists = []
+    data.forEach(player => {
+      if (player.Assists >= 2) {
+        topPlayersAssists.push(player.Player)
+      }
+    })
+    return topPlayersAssists
 }
 
 /**
@@ -25,5 +37,11 @@ export function getTopPlayersAssists (data) {
  * @returns {string[]} The names of the players with at least 3 goals + assists in the tournament
  */
 export function getTopPlayersGoalsAndAssists (data) {
-
+    const topPlayersGoalsAndAssists = []
+    data.forEach(player => {
+      if (player.GoalsAndAssists >= 3) {
+        topPlayersGoalsAndAssists.push(player.Player)
+      }
+    })
+    return topPlayersGoalsAndAssists
 }
