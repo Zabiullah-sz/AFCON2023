d27337604554441099dc1f3657b370c1e8e1c400
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-04-10 16:19:25 -0400

    Viz 4 et 5: Refactor titre et axes

--- js/viz2/legend.js
+++ js/viz2/legend.js
@@ -2,23 +2,24 @@
  * Draws the legend.
  *
  * @param {*} g The d3 Selection of the graph's g SVG element
+ * @param {object[]} data The legend data
  */
 export function drawLegend (g, data) {
-    const legendItems = g.selectAll('legendItem')
-    .data(data)
-    .enter().append('g')
-    .attr('class', 'legendItem')
-    .attr('transform', (d, i) => `translate(${i * 150}, -75)`)
+  const legendItems = g.selectAll('legendItem')
+  .data(data)
+  .enter().append('g')
+  .attr('class', 'legendItem')
+  .attr('transform', (d, i) => `translate(${i * 150}, -75)`)
 
-    legendItems.append('rect')
-    .attr('width', 18)
-    .attr('height', 18)
-    .style('fill', d => d.color)
+  legendItems.append('rect')
+  .attr('width', 18)
+  .attr('height', 18)
+  .style('fill', d => d.color)
 
-    legendItems.append('text')
-    .attr('x', 24)
-    .attr('y', 9)
-    .attr('dy', '.35em')
-    .text(d => d.label)
-    .style('fill', '#000')
-  }
+  legendItems.append('text')
+  .attr('x', 24)
+  .attr('y', 9)
+  .attr('dy', '.35em')
+  .text(d => d.label)
+  .style('fill', '#000')
+}
