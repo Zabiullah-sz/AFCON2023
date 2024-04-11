82cfb28525171c3ede5585c0b274a817b6239bf2
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-04-11 15:00:42 -0400

    Viz3: Ajustement légende

--- js/viz3/legend.js
+++ js/viz3/legend.js
@@ -7,28 +7,19 @@ import d3Legend from 'd3-svg-legend'
  * @param {*} g The d3 Selection of the SVG g element containing the legend
  */
 export function drawLegend (colorScale, g) {
-
-
-  const sortedDomain = colorScale.domain().sort();
-
   const legend = d3Legend.legendColor()
     .shape('circle')
     .scale(colorScale)
 
-
   g.append('g')
     .attr('class', 'legend')
-    .attr('font-family', 'Open Sans Condensed')
-    .attr('font-size', 18)
-    .attr('transform', 'translate(560, 150)')
-
+    .attr('font-size', 16)
+    .attr('transform', 'translate(510, 150)')
     .call(legend)
 
   g.append('text')
     .attr('class', 'legend-title')
-    .attr('font-family', 'Open Sans Condensed')
-    .attr('font-size', 18)
-    .attr('transform', 'translate(550, 130)')
-
+    .attr('font-size', 16)
+    .attr('transform', 'translate(500, 130)')
     .text('Légende')
 }
