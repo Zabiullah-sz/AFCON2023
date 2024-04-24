6a77fbeb1f313af5520f66c791f04125f0f3b3cc
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-04-24 19:27:48 -0400

    Viz 4 et 5: Refactor main

--- js/viz4/main.js
+++ js/viz4/main.js
@@ -4,28 +4,17 @@ import playerData from '../../assets/data/defense_player.csv'
 import * as d3 from 'd3';
 import { createScatterPlot } from './viz.js';
 
-// Define the main function to initialize the visualization
 export function initializeVisualization4() {
-  // Load the CSV data
   Promise.all([
     d3.csv(teamData),
     d3.csv(playerData)
   ]).then(function(data) {
     const teamData = data[0]; // Data from defense_team.csv
-    const playerData = data[1];
-    // Convert string numbers to numeric values
-    // Specify dimensions for the scatter plot
+    const playerData = data[1]; // Data from defense_player.csv
+
     const width = 750;
     const height = 600;
 
-    // Create the scatter plot using the data and dimensions
     createScatterPlot(teamData, playerData, width, height);
-
-    // Optional: Add event listeners or other interactions here
-    // For example, you can add a click handler to update the plot
-
-  }).catch(function(error) {
-    // Handle data loading errors
-    console.error('Error loading data:', error);
   });
 }
--- js/viz5/main.js
+++ js/viz5/main.js
@@ -1,28 +1,14 @@
+'use strict';
 
-import playerData from '../../assets/data/offense_team.csv'
 import * as d3 from 'd3';
+import playerData from '../../assets/data/offense_team.csv'
 import { createScatterPlot } from './viz.js';
 
-// Define the main function to initialize the visualization
 export function initializeVisualization5() {
-  // Load the CSV data
-  Promise.all([
-    d3.csv(playerData)
-  ]).then(function(data) {
-    const playerData = data[0]; //Data from defense_team.csv
-    // Convert string numbers to numeric values
-    // Specify dimensions for the scatter plot
+  d3.csv(playerData, d3.autoType).then(function(data) {
     const width = 750;
     const height = 600;
 
-    // Create the scatter plot using the data and dimensions
-    createScatterPlot(playerData, width, height);
-
-    // Optional: Add event listeners or other interactions here
-    // For example, you can add a click handler to update the plot
-
-  }).catch(function(error) {
-    // Handle data loading errors
-    console.error('Error loading data:', error);
+    createScatterPlot(data, width, height);
   });
 }
